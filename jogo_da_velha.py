162c9af09c934c4557ce249be653c786e6b0b98b
GuiCastro7 <d2024010605@unifei.edu.br>
2024-09-30 09:52:11 -0300

    jogo_da_velha.py

--- jogo_da_velha.py
+++ jogo_da_velha.py
@@ -1,16 +1,40 @@
-from PyQt5 import QtWidgets
+from PyQt5 import QtCore, QtWidgets
 
-class JogoDaVelha:
+class TicTacToeGame(QtWidgets.QWidget):
     def __init__(self):
+        super().__init__()
         self.player_turn = 'X'
         self.board = [['' for _ in range(3)] for _ in range(3)]
+        self.setup_ui()
+
+    def setup_ui(self):
+        self.setGeometry(180, 110, 581, 371)
+        self.gridLayout = QtWidgets.QGridLayout(self)
+        self.buttons = {}
+        
+        for i in range(3):
+            for j in range(3):
+                button = QtWidgets.QPushButton("")
+                button.setFixedSize(100, 100)
+                button.clicked.connect(lambda checked, x=i, y=j: self.make_move(x, y))
+                self.buttons[(i, j)] = button
+                self.gridLayout.addWidget(button, i, j)
+
+        self.label = QtWidgets.QLabel("Vencedor:")
+        self.gridLayout.addWidget(self.label, 3, 0, 1, 3)
+
+        # Reset button
+        self.reset_button = QtWidgets.QPushButton("Reset")
+        self.reset_button.clicked.connect(self.reset_game)
+        self.gridLayout.addWidget(self.reset_button, 4, 0, 1, 3)
 
     def make_move(self, x, y):
         if self.board[x][y] == '' and not self.check_winner():
             self.board[x][y] = self.player_turn
+            self.buttons[(x, y)].setText(self.player_turn)
+            if self.check_winner():
+                self.label.setText(f"Vencedor: {self.player_turn}")
             self.player_turn = 'O' if self.player_turn == 'X' else 'X'
-            return self.check_winner()
-        return False
 
     def check_winner(self):
         for line in self.board:
@@ -28,3 +52,6 @@ class JogoDaVelha:
     def reset_game(self):
         self.player_turn = 'X'
         self.board = [['' for _ in range(3)] for _ in range(3)]
+        self.label.setText("Vencedor:")
+        for button in self.buttons.values():
+            button.setText("")
