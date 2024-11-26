1a179733bfd06a4c5f30be51b79ffad2105af214
GuiCastro7 <d2024010605@unifei.edu.br>
2024-11-26 15:57:53 -0300

    trabalho_arvore_lista.py

--- trabalho_arvore_lista.py
+++ trabalho_arvore_lista.py
@@ -1,27 +1,27 @@
 import random
 
-class No:
-    def __init__(self, valor):
-        self.valor = valor
-        self.prox = None  # Para lista
-        self.esquerda = None  # Para árvore
-        self.direita = None   # Para árvore
+class No: # Usado para as duas classes
+    def __init__(self, num):
+        self.num = num
+        self.prox = None  # Utilizado para o funcionamento da lista
+        self.esquerda = None  # Usado na árvore
+        self.direita = None   # Usado na árvore
 
 class Lista:
     def __init__(self):
         self.ini = None
         self.qtdLista = 0
     
-    def buscar(self, valor):
+    def buscar(self, num):
         # Busca linear na lista
         atual = self.ini
         busca = 0
         while atual:
             busca += 1
-            if atual.valor == valor:
-                return busca  # Encontrou o valor
+            if atual.num == num:
+                return busca  # Se encontrar o número, retorna o número de iterações 
             atual = atual.proximo
-        return busca  # Retorna o número de iterações (não encontrou)
+        return busca  # Se não encontrar, retorna o número de busca
     
     def insere_lista(self, num):
         novo_no = No(num)
@@ -32,12 +32,12 @@ class Lista:
 
         aux = self.ini
         while(True):
-            if aux.prox == None or aux.prox.valor > num:
+            if aux.prox == None or aux.prox.num > num:
                 break
             aux = aux.prox
             self.qtdLista += 1
 
-        if aux.prox == None and num > aux.valor: #último elemento
+        if aux.prox == None and num > aux.num: #último elemento
             aux.prox = novo_no
         elif aux == self.ini: #primeiro elemento
             novo_no.prox = self.ini
@@ -51,70 +51,73 @@ class Arvore:
         self.raiz = None
         self.qtdArvore = 0
     
-    def buscar(self, valor, no):
+    def buscar(self, num):
         # Busca binária na árvore
+        atual = self.raiz
         busca = 0
-        while no:
+        while atual:
             busca += 1
-            if valor < no.valor:
-                no = no.esquerda
-            elif valor > no.valor:
-                no = no.direita
+            if num < atual.num:
+                atual = atual.esquerda
+            elif num > atual.num:
+                atual = atual.direita
             else:
-                return busca  # Encontrou o valor
-        return busca  # Não encontrou o valor
+                return busca  # Encontrou o número
+        return busca  # Não encontrou o número
     
-    def insere_arvore(self, valor):
-        # Conta a quantidade de buscas feitas ao tentar inserir
-        busca = self.buscar(valor, self.raiz)
-        self.qtdArvore += busca  # Soma as buscas feitas
-
-        if busca == 0:  # O número não está na árvore
-            novo_no = No(valor)
-            if not self.raiz:
-                self.raiz = novo_no
-            else:
-                self._inserir_no(self.raiz, novo_no)
+    def insere_arvore(self, num):
+        self.qtdArvore += self.buscar(num)
+        
+        # Caso a árvore esteja vazia, insere o primeiro nó
+        if self.raiz == None:
+            self.raiz = No(num)
             return True
-        return False
-    
-    def _inserir_no(self, no_atual, novo_no):
-        # Método auxiliar para inserir recursivamente na árvore
-        self.qtdArvore += 1
-        if novo_no.valor < no_atual.valor:
-            if no_atual.esquerda:
-                self._inserir_no(no_atual.esquerda, novo_no)
-            else:
-                no_atual.esquerda = novo_no
-        else:
-            if no_atual.direita:
-                self._inserir_no(no_atual.direita, novo_no)
-            else:
-                no_atual.direita = novo_no
 
-def gerar_numeros_unicos():
+        
+        atual = self.raiz
+        while True:
+            self.qtdArvore += 1  # Incrementa a contagem de buscas realizadas
+            if num < atual.num:
+                if atual.esquerda:
+                    atual = atual.esquerda
+                else:
+                    atual.esquerda = No(num)
+                    return True
+            elif num > atual.num:
+                if atual.direita:
+                    atual = atual.direita
+                else:
+                    atual.direita = No(num)
+                    return True
+            else:
+                # Se o núemro ja existir na árvore, não acontece a inserção
+                return False
+
+def geracao_numeros_unicos():
     numeros = []
-    numeros_gerados = set()  # Para verificar unicidade
+    numeros_gerados = set() #Verifica a unicidade
     
-    while len(numeros) < 5000:
-        numero = random.randint(0, 100000)
-        if numero not in numeros_gerados:
-            numeros_gerados.add(numero)
-            numeros.append(numero)
+    for i in range(5000):  # Gera exatamente 5000 números
+        while True:
+            numero = random.randint(0, 100000)
+            if numero in numeros_gerados:
+                break  # Sai do loop e gera outro número
+            else:
+                numeros_gerados.add(numero)
+                numeros.append(numero)
+                break #Sai do loop porque o número foi encontrado e adicionado
     
     return numeros
 
-def main():
-    lista = Lista()
-    arvore = Arvore()
-    numeros_gerados = gerar_numeros_unicos()  # Gera 5000 números únicos
 
-    for numero in numeros_gerados:
-        lista.insere_lista(numero)
-        arvore.insere_arvore(numero)
+#Aplicação dos numeros na lista e arvore. 
+lista = Lista()
+arvore = Arvore()
+numeros_gerados = geracao_numeros_unicos()  # Gera 5000 números únicos
 
-    print(f"Quantidade total de buscas na lista: {lista.qtdLista}")
-    print(f"Quantidade total de buscas na árvore: {arvore.qtdArvore}")
+for numero in numeros_gerados:
+    lista.insere_lista(numero)
+    arvore.insere_arvore(numero)
 
-if __name__ == "__main__":
-    main()
+#Impressão sobre a quantidade de buscas feitas tanto na lista quanto no código e qual a diferença(numérica) nesse caso
+print("Quantidade total de buscas obtidas na lista e na árvore foram:\nLista: {} \nÁrvore: {}".format(lista.qtdLista, arvore.qtdArvore))
