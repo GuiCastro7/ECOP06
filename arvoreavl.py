10d6989781cf734ce36d604f6057b565fafceed3
GuiCastro7 <d2024010605@unifei.edu.br>
2024-12-03 10:43:35 -0300

    arvoreavl.py

--- arvoreavl.py
+++ arvoreavl.py
@@ -1,20 +1,20 @@
 class No:
-    def __init__(self, key):
-        self.key = key
+    def __init__(self, num):
+        self.num = num
         self.esquerda = None
         self.direita = None
         self.altura = 1  # altura inicial de cada nó é 1 (porque um nó isolado tem altura 1)
 
 class ArvoreAVL:
     def __init__(self):
-        self.root = None
+        self.raiz = None
 
     def altura(self, no):
         if not no:
             return 0
         return no.altura
 
-    def balance_factor(self, no):
+    def balanceamento(self, no):
         if not no:
             return 0
         return self.altura(no.esquerda) - self.altura(no.direita)
@@ -47,51 +47,51 @@ class ArvoreAVL:
 
         return y
 
-    def insere(self, root, key):
-        if not root:
-            return No(key)
+    def insere(self, raiz, num):
+        if not raiz:
+            return No(num)
 
         # Inserir no lugar correto
-        if key < root.key:
-            root.esquerda = self.insere(root.esquerda, key)
+        if num < raiz.num:
+            raiz.esquerda = self.insere(raiz.esquerda, num)
         else:
-            root.direita = self.insere(root.direita, key)
+            raiz.direita = self.insere(raiz.direita, num)
 
         # Atualiza a altura do nó ancestral
-        root.altura = 1 + max(self.altura(root.esquerda), self.altura(root.direita))
+        raiz.altura = 1 + max(self.altura(raiz.esquerda), self.altura(raiz.direita))
 
         # Verificar o fator de balanceamento para garantir que a árvore se mantenha balanceada
-        balance = self.balance_factor(root)
+        balance = self.balanceamento(raiz)
 
         # Caso 1: Rotação à direita
-        if balance > 1 and key < root.esquerda.key:
-            return self.direita_rotação(root)
+        if balance > 1 and num < raiz.esquerda.num:
+            return self.direita_rotação(raiz)
 
         # Caso 2: Rotação à esquerda
-        if balance < -1 and key > root.direita.key:
-            return self.esquerda_rotação(root)
+        if balance < -1 and num > raiz.direita.num:
+            return self.esquerda_rotação(raiz)
 
         # Caso 3: Rotação à esquerda-direita
-        if balance > 1 and key > root.esquerda.key:
-            root.esquerda = self.esquerda_rotação(root.esquerda)
-            return self.direita_rotação(root)
+        if balance > 1 and num > raiz.esquerda.num:
+            raiz.esquerda = self.esquerda_rotação(raiz.esquerda)
+            return self.direita_rotação(raiz)
 
         # Caso 4: Rotação à direita-esquerda
-        if balance < -1 and key < root.direita.key:
-            root.direita = self.direita_rotação(root.direita)
-            return self.esquerda_rotação(root)
+        if balance < -1 and num < raiz.direita.num:
+            raiz.direita = self.direita_rotação(raiz.direita)
+            return self.esquerda_rotação(raiz)
 
-        return root
+        return raiz
 
-    def insere_key(self, key):
-        self.root = self.insere(self.root, key)
+    def insere_key(self, num):
+        self.raiz = self.insere(self.raiz, num)
 
     # Metodo de exibir em ordem a arvore AVL (linear)
-    def ordem(self, root):
-        if root:
-            self.ordem(root.esquerda)
-            print(root.key, end=" ")
-            self.ordem(root.direita)
+    def ordem(self, raiz):
+        if raiz:
+            self.ordem(raiz.esquerda)
+            print(raiz.num, end=" ")
+            self.ordem(raiz.direita)
 
     # Método para exibir a árvore de maneira hierárquica (raízes)
     def arvore_hierárquica(self, no, level=0):
@@ -99,7 +99,7 @@ class ArvoreAVL:
             # Primeiro imprime a subárvore direita, para posicioná-la no topo visual
             self.arvore_hierárquica(no.direita, level + 1)
             # Imprime o nó atual, com indentação proporcional ao nível
-            print(" " * (level * 8) + str(no.key))
+            print(" " * (level * 8) + str(no.num))
             # Depois imprime a subárvore esquerda
             self.arvore_hierárquica(no.esquerda, level + 1)
 
@@ -113,10 +113,10 @@ for val in valores:
 
 # Exibindo a árvore AVL em ordem
 print("Árvore AVL em ordem :")
-avl.ordem(avl.root)
+avl.ordem(avl.raiz)
 print("\n")
 
 # Exibindo a árvore com a estrutura hierárquica (raízes e filhos)
 print("Visualização de uma arvore AVL hierarquicamente:")
-avl.arvore_hierárquica(avl.root)
+avl.arvore_hierárquica(avl.raiz)
 print("\n")
